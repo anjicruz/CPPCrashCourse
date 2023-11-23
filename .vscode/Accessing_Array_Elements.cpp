41d6ba9fca4445dfd4a2768afe32767e8dc308b3
anji <anji_cruz@msn.com>
2023-11-23 22:55:38 +0800

    Accessing array elements using cout 11/23

--- .vscode/Accessing_Array_Elements.cpp
+++ .vscode/Accessing_Array_Elements.cpp
@@ -1,9 +1,11 @@
 #include <cstdio>
+#include <iostream> // this must be included for cout to be recognized
 
 int main() {
-    int arr[] = {1, 2, 3, 4};
-    printf("The third element is %d.\n", arr[2]);
-    arr[2] = 100;
-    printf("The third element is %d.\n", arr[2]);
+    int arr[4] = {};
+    std::cout << "The second element of an array is: " << arr[2];
 }
-
+    //     printf("The third element is %d.\n", arr[2]);
+    //     arr[2] = 100;
+    //     printf("The third element is %d.\n", arr[2]);
+    // }
