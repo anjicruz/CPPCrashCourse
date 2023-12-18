4da7ac006513bb8a8d15dd879b3c77962d4f7db9
anji <anji_cruz@msn.com>
2023-12-19 03:21:12 +0800

    if_statements 12/19

---
+++ test_program.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// using namespace std;
+
+int main()
+{
+    std::cout << "Please enter two integer values: " << std::endl;
+    int val1;
+    int val2;
+    if (val1!=val2)
+    {
+        std::cout << "val2" << std::endl;
+    }
+    
+}/*Write a program that prompts the user to enter two integer values. Store these values in int variables named val1 and val2. Write your program to determine the smaller, larger, sum, +
+difference, product, and ratio of these values and report them to the user.*/
